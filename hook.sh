21fc2055a8e897603ffc3baec449e0d2fc83a9d4
changye.zeng <changye.zeng@iMac-9.local>
2017-03-17 10:35:06 +0800

    增加一个打包脚本

---
+++ hook.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+echo "复制修改过的类到apktool工程..."
+cp -f src/main/java/brut/androlib/Androlib.java apktool/brut.apktool/apktool-lib/src/main/java/brut/androlib/Androlib.java
+cp -f src/main/java/brut/androlib/ApkDecoder.java apktool/brut.apktool/apktool-lib/src/main/java/brut/androlib/ApkDecoder.java
+cp -f src/main/java/brut/androlib/ApkOptions.java apktool/brut.apktool/apktool-lib/src/main/java/brut/androlib/ApkOptions.java
+cp -f src/main/java/brut/androlib/res/AndrolibResources.java apktool/brut.apktool/apktool-lib/src/main/java/brut/androlib/res/AndrolibResources.java
+cp -f src/main/java/brut/androlib/src/SmaliBuilder.java apktool/brut.apktool/apktool-lib/src/main/java/brut/androlib/src/SmaliBuilder.java
+cp -f src/main/java/brut/apktool/Main.java apktool/brut.apktool/apktool-cli/src/main/java/brut/apktool/Main.java
+
+echo "构建jar"
+cd apktool
+gradle fatjar
+cd ..
+
+echo "把构建出来的jar复制到根目录下"
+cp -f apktool/brut.apktool/apktool-cli/build/libs/apktool-cli.jar ./temp/
+
+if [ "$1" = "-r" ]; then
+    echo "回滚apktool工程被修改的类"
+    cd apktool
+    git checkout -- brut.apktool/apktool-lib/src/main/java/brut/androlib/Androlib.java
+    git checkout -- brut.apktool/apktool-lib/src/main/java/brut/androlib/ApkDecoder.java
+    git checkout -- brut.apktool/apktool-lib/src/main/java/brut/androlib/ApkOptions.java
+    git checkout -- brut.apktool/apktool-lib/src/main/java/brut/androlib/res/AndrolibResources.java
+    git checkout -- brut.apktool/apktool-lib/src/main/java/brut/androlib/src/SmaliBuilder.java
+    git checkout -- brut.apktool/apktool-cli/src/main/java/brut/apktool/Main.java
+    cd ..
+fi
+
+echo "done"
